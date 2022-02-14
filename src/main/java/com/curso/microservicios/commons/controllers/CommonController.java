f97bf6e036b01848a4e5fae8e41ccfee18ef5e56
Abel Antunez de la Fuente <abel.antunez@tink.com>
2022-02-14 10:26:51 +0100

    Push cambio de pc

--- src/main/java/com/curso/microservicios/commons/controllers/CommonController.java
+++ src/main/java/com/curso/microservicios/commons/controllers/CommonController.java
@@ -1,9 +1,14 @@
 package com.curso.microservicios.commons.controllers;
 
 import com.curso.microservicios.commons.services.CommonService;
+import java.util.HashMap;
+import java.util.Map;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -18,6 +23,11 @@ public class CommonController<E, S extends CommonService<E>> {
         return ResponseEntity.ok().body(service.findAll());
     }
 
+    @GetMapping("/pagina")
+    public ResponseEntity<?> listar(Pageable pageable) {
+        return ResponseEntity.ok().body(service.findAll(pageable));
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<?> ver(@PathVariable Long id) {
         Optional<E> o = service.findById(id);
@@ -28,7 +38,10 @@ public class CommonController<E, S extends CommonService<E>> {
     }
 
     @PostMapping
-    public ResponseEntity<?> crear(@RequestBody E entity) {
+    public ResponseEntity<?> crear(@Valid @RequestBody E entity, BindingResult result) {
+        if (result.hasErrors()) {
+            return this.validar(result);
+        }
         E entityDb = service.save(entity);
         return ResponseEntity.status(HttpStatus.CREATED).body(entityDb);
     }
@@ -37,5 +50,13 @@ public class CommonController<E, S extends CommonService<E>> {
     public ResponseEntity<?> eliminar(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    protected ResponseEntity<?> validar(BindingResult result) {
+        Map<String, Object> errores = new HashMap<>();
+        result.getFieldErrors().forEach(err ->{
+            errores.put(err.getField(), " El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errores);
     }
 }
--- src/main/java/com/curso/microservicios/commons/services/CommonService.java
+++ src/main/java/com/curso/microservicios/commons/services/CommonService.java
@@ -1,10 +1,14 @@
 package com.curso.microservicios.commons.services;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CommonService<E> {
 
     Iterable<E> findAll();
+
+    Page<E> findAll(Pageable pageable);
 
     Optional<E> findById(Long id);
 
